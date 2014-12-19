84f05d489d82a113ae9967b240394b8b397334f5
Isaiah Basu <isaiahbasu@gmail.com>
2014-12-19 09:51:13 -0800

    This is how far I got
    Couldn't get the buttons to work :/

--- app/src/main/java/android/basui/finalproject/start_game.java
+++ app/src/main/java/android/basui/finalproject/start_game.java
@@ -1,5 +1,21 @@
 package android.basui.finalproject;
 
-public class start_game {
+import android.view.View;
+import android.widget.Button;
 
+public class start_game {
+    Button button = (Button) findViewById(R.id.button);
+    button.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            // Do something in response to button click
+
+        }
+    }
+    Button button = (Button) findViewById(R.id.button2);
+    button.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View); {
+            // Do something in response to button click
+            
+        }
+    }
 }
